2b02e92f805349a3dfb02cb22f25bf6e8d72ee5e
laucavv <laucavv23@gmail.com>
2020-02-05 21:23:29 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
@@ -6,7 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{   
-	puts("\"Programming is like building a multilingual puzzle\n");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
