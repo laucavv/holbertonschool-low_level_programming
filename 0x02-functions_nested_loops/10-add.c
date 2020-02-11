1d63660b1f05875f0676d67b0af9b164d623c7ee
laucavv <laucavv23@gmail.com>
2020-02-11 17:50:38 +0000

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *add -  function that adds two integers and returns the result.
+ *@c: values
+ *@n: values
+ * Return: suma
+ */
+
+int add(int c, int n)
+{
+	int suma;
+
+	suma = c + n;
+	return (suma);
+}
+
