dac58e64b50740bd500acb274ba04727b68daddf
laucavv <laucavv23@gmail.com>
2020-02-11 14:11:08 +0000

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+* print_alphabet -  prints the alphabet, in lowercase, followed by a new line.
+* Return:0
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
