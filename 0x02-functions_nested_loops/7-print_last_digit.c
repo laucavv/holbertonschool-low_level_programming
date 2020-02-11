4d169912bcc5da3705fdedb15a3f01267a2193bf
laucavv <laucavv23@gmail.com>
2020-02-11 14:27:53 +0000

    last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit- computes the absolute value of an integer.
+ *@c: value
+ * Return: num
+ */
+int print_last_digit(int c)
+{
+	int num;
+
+	if (c < 0)
+	{
+		c = c * -1;
+	}
+
+	num = c % 10;
+
+	_putchar(num + '0');
+	return (num);
+
+
+}
