9d4d5d0ffb79d15f26134029a18ed8738c440e83
laucavv <laucavv23@gmail.com>
2020-02-13 15:37:05 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - function that checks for uppercase character.
+ *
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
