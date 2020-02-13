97b32ea327c1f270fd2da22a10d280cad3d2c57b
laucavv <laucavv23@gmail.com>
2020-02-13 15:47:49 +0000

    most_numbers.

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers -  function that prints the number
+ *
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
