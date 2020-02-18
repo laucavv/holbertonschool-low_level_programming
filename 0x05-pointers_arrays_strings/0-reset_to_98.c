e5975167ba6eebfac8968ba11345cbb3fb8cd760
laucavv <laucavv23@gmail.com>
2020-02-18 14:12:08 +0000

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 -  updates the value it points to to 98.
+ *@n: value
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
