527a05c498f7797d288e364df6a8bbbb672939d0
laucavv <laucavv23@gmail.com>
2020-02-24 15:56:06 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_memset - function that fills memory with a constant byte.
+ *@s: buffer
+ *@b: constant byte
+ *@n: size
+ *
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
