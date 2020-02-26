3803fd5856213eef1de9d8a2c14747a7cb22d946
laucavv <laucavv23@gmail.com>
2020-02-26 23:59:26 +0000

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,33 @@
 #include "holberton.h"
 /**
-*_sqrt_recursion - square root of a number
-*@n: num
-*Return: square
-*/
+ *increase - search the square.
+ *@n: num
+ *@x: incremento
+ *Return: value
+ */
+int increase(int n, int x)
+{
+	int value = 0;
 
+	if ((x * x) == n)
+	{
+		value = x;
+	}
+	else if ((x * x) < n)
+	{
+		return (increase(n, (x + 1)));
+	}
+	else if ((x * x) > n)
+	{
+		return (-1);
+	}
+	return (value);
+}
+/**
+ *_sqrt_recursion - square root of a number
+ *@n: num
+ *Return: square
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
@@ -12,26 +35,4 @@ int _sqrt_recursion(int n)
 		return (-1);
 	}
 	return (increase(n, 0));
-}
-
-/**
-*increase - search the square.
-*@n: num
-*@x: incremento
-*Return: value
-*/
-int increase(int n, int x)
-{
-	if ((x * x) == n)
-	{
-		return (x);
-	}
-	else if ((x * x) < n)
-	{
-		increase(n, (x + 1));
-	}
-	else
-	{
-		return (-1);
-	}
 }
