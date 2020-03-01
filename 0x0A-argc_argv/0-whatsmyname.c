fe1bcea40e9bf9bea3642b6c69ca9a084f687dcb
laucavv <laucavv23@gmail.com>
2020-03-01 01:58:41 +0000

    my_name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*main -  prints its name, followed by a new line.
+*@argc: arguments
+*@argv: pointer arguments
+*Return: 0;
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
