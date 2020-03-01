777280f69a259710ce941b4840c71827bc91b3e7
laucavv <laucavv23@gmail.com>
2020-03-01 04:26:02 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+*_isdigit - program that multiplies two numbers.
+*@c: arguments
+*Return: 0 or 1;
+*/
+int _isdigit(char *c)
+{
+	int i, res;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (isdigit(c[i]) == 0)
+		{
+			res = 0;
+		}
+		else
+		{
+			res = 1;
+		}
+	}
+	return (res);
+}
+/**
+*main - program that adds positive numbers.
+*@argc: arguments
+*@argv: pointer
+*Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc < 1)
+	{
+		printf("0");
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			sum = sum + atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
