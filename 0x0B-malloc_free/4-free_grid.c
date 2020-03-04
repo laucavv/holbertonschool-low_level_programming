77c81e347545c888ad52d7bdf8b7d1981c9422e6
laucavv <laucavv23@gmail.com>
2020-03-04 03:14:23 +0000

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *free_grid - free alloc_grid
+ *@grid: array
+ *@height: size
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
