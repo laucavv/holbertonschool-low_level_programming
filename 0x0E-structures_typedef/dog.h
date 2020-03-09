a9990f0f74334aadde43f6b2697930ffa2d8722e
laucavv <laucavv23@gmail.com>
2020-03-09 18:45:05 +0000

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - define a new type
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: you can create a new type of data
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
