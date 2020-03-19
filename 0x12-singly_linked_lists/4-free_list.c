4895146e81d8b09223782094c24e00773b2c0791
laucavv <laucavv23@gmail.com>
2020-03-19 22:46:06 +0000

    free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list -  function that frees a list.
+ * @head: first item in the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *aux, *auxend;
+
+	aux = head;
+
+	while (p != NULL)
+	{
+		auxend = aux->next;
+		free(aux->str);
+		free(aux->next);
+		aux = auxend;
+	}
+}
