2edcad7954b14a53dd3839bd3252e634bc74f4b0
laucavv <laucavv23@gmail.com>
2020-03-19 23:40:53 +0000

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *print_before - before the main function is executed.
+ *print_first - before the main function is executed.
  */
-void print_before(void)
+void print_first(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
