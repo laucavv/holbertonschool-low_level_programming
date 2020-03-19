e37b6020ca3a3e3cb515b5eaec2e0fc1c8daa2da
laucavv <laucavv23@gmail.com>
2020-03-19 23:32:18 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ *print_before - before the main function is executed.
+ */
+void print_before(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
