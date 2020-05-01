0cbde94eb28d8be8e720a278aca64fc8baa1d1f5
laucavv <laucavv23@gmail.com>
2020-05-01 20:49:58 +0000

    insert_node_index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - check the code for Holberton School students.
+ * @h:list
+ * @idx:index
+ * @n:value
+ * Return: Always 0.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *node = NULL, *tmp = NULL, *aux = NULL;
+
+	node = malloc(sizeof(dlistint_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->n = n;
+	tmp = *h;
+	if (idx == 0)
+	{
+		node->prev = NULL;
+		node->next = tmp;
+		*h = node;
+		return (node);
+	}
+
+	while (tmp)
+	{
+		if (i == (idx - 1))
+		{
+			aux = tmp->next;
+			node->next = aux;
+			node->prev = tmp;
+			tmp->next = node;
+			aux->prev = node;
+			return (node);
+		}
+		i++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+
+
+}
