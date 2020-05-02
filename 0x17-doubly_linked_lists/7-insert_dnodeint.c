ce321d787cff7a0fdcbb696c649dd9208e3f1d39
laucavv <laucavv23@gmail.com>
2020-05-02 18:42:58 +0000

    fix6

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (node == NULL)
 		return (NULL);
 	node->n = n;
-	if (idx == 0)
+	if (idx == 0 || tmp == NULL)
 	{
 		if (tmp == NULL)
 			node->next = NULL;
