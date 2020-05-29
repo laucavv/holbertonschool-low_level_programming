b26fbaf407d18a5b8a262a1503eaae77e82d84d9
laucavv <laucavv23@gmail.com>
2020-05-29 08:02:56 +0000

    delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete -  function that prints a hash table.
+ * @ht: is the hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp = NULL, *aux = NULL;
+	unsigned long int index = 0;
+
+	if (ht && ht->array)
+	{
+
+		for (index = 0; index < ht->size; index++)
+		{
+			tmp = ht->array[index];
+			while (tmp)
+			{
+				aux = tmp->next;
+				free(tmp->key);
+				free(tmp->value);
+				free(tmp);
+				tmp = aux;
+			}
+		}
+		free(ht->array);
+		free(ht);
+	}
+
+}
